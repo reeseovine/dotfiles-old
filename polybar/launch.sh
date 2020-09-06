a9a48029a841bcfb61112c4153b24b875f49f6cb
Reese Jacobson <me@katacarbix.xyz>
2020-09-05 20:21:28 -0500

    hooo boy there's a lot of stuff in this one
    - fleshed out backup script into a whole system of 3 scripts
    - trimmed down polybar configs a little bit
    - fixed some filepaths that got broken during my git folder reorganizing

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
 # wait
 sleep 2
 
-# Launch bar1 and bar2
+# Launch bar
 polybar -c ~/.config/polybar/config.ini main &
