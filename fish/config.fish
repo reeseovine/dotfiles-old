4671b19389df355a2482db69f4119c91053f5e87
Reese Jacobson <me@katacarbix.xyz>
2021-02-26 05:06:56 -0600

    24-bit color fix in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -14,10 +14,8 @@ set -g theme_color_scheme terminal2-light
 set -g theme_nerd_fonts yes
 
 # fix for SSH colors - https://github.com/fish-shell/fish-shell/issues/7701
-# enable 24-bit color when supported
-if test "$COLORTERM" = truecolor
-    set -g fish_term24bit 1
-end
+# always enable 24-bit color
+set -g fish_term24bit 1
 
 # Load autojump
 source /usr/share/autojump/autojump.fish
